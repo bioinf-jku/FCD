c7e7e9968bf19dd42ecb1b172f4d8cd228493a3a
Philipp Renz <renz.ph@gmail.com>
2023-08-22 23:39:54 +0200

    Port to pytorch

---
+++ fcd_torch/__init__.py
@@ -0,0 +1,4 @@
+from .fcd import get_fcd, get_predictions, load_ref_model
+from .utils import calculate_frechet_distance, canonical_smiles
+
+__version__ = "1.2"
--- setup.py
+++ setup.py
@@ -3,27 +3,20 @@
 # from distutils.core import setup
 from setuptools import setup
 
-with open("README.md", "r", encoding='utf8') as fh:
+with open("README.md", "r", encoding="utf8") as fh:
     long_description = fh.read()
 
-setup(name='FCD',
-      version='1.1',
-      author='',
-      author_email='',
-      description='Fréchet ChEMNet Distance',
-      url='https://github.com/bioinf-jku/FCD',
-      packages=['fcd'],
-      license='LGPLv3',
-      long_description=long_description,
-      long_description_content_type="text/markdown",
-      install_requires=[
-          'keras',
-          'numpy',
-          'scipy',
-          'tensorflow'
-      ],
-      extras_require={
-          'rdkit': ['rdkit'],
-      },
-      include_package_data=True,
-      )
+setup(
+    name="FCD",
+    version="1.2",
+    author="Philipp Renz",
+    author_email="renz.ph@gmail.com",
+    description="Fréchet ChEMNet Distance",
+    url="https://github.com/bioinf-jku/FCD",
+    packages=["fcd"],
+    license="LGPLv3",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    install_requires=["torch", "numpy", "scipy", "rdkit"],
+    include_package_data=True,
+)
